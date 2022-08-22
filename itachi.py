0ca5aee1d0f1b2913f2f4c090f015dfbf8b34a1b
Mirza Sohaib Baig <107631355+MirzaSohaib-Baig@users.noreply.github.com>
2022-08-22 19:40:00 +0500

    Add files via upload

---
+++ itachi.py
@@ -0,0 +1,76 @@
+import turtle as t
+
+#eye color
+t.title("Itachi's Mangekyo Sharingan")
+t.bgcolor('black')
+t.speed(5)
+t.penup()
+t.color("black","red")
+t.pensize(5)
+t.goto(0,200)
+t.pendown()
+t.begin_fill()
+t.circle(-200)
+t.end_fill()
+t.right(5)
+t.forward(10)
+t.right(145)
+
+#black leaves
+t.color("black","black")
+t.begin_fill()
+for i in range(40):
+  t.forward(7)
+  t.left(2)
+t.right(95)
+for i in range(24):
+  t.forward(5)
+  t.right(2)
+t.left(127)
+for i in range(20):
+  t.forward(2)
+  t.left(1)
+t.left(25)
+for i in range(45):
+  t.forward(7)
+  t.left(2)
+t.right(95)
+for i in range(21):
+  t.forward(5)
+  t.right(2)
+t.left(127)
+for i in range(20):
+  t.forward(2)
+  t.left(1)
+t.left(25)
+for i in range(45):
+  t.forward(7)
+  t.left(2)
+t.right(95)
+for i in range(20):
+  t.forward(5)
+  t.right(2)
+t.left(127)
+for i in range(20):
+  t.forward(2)
+  t.left(1)
+t.end_fill()
+
+#internal circle
+t.penup()
+t.goto(0,40)
+t.color("black","red")
+t.begin_fill()
+t.circle(30)
+t.end_fill()
+t.hideturtle()
+t.done()
+
+  
+  
+  
+  
+  
+  
+  
+  
